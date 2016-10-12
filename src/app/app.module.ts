70c2e792cf425ab6cbc4684c0d9ecc641de921bf
cbalit <dou4872>
2016-10-12 16:57:49 +0200

    step-01

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
+// CORE DEPS
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
+// MATERIAL DESIGN MODULES
+import { MaterialModule } from '@angular/material';
 
-import { AppComponent } from './app.component';
+
+// import { PeopleAppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    MaterialModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    // PeopleAppComponent
+  ],
+  providers: [ ],
+  bootstrap: [
+    // PeopleAppComponent
+  ]
 })
 export class AppModule { }
