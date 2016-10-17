b71c2b2f10d2e6c16bb866d31cf4b48b152db8fc
cbalit <dou4872>
2016-10-17 14:03:43 +0200

    step-07-solution

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 // MATERIAL DESIGN MODULES
 import { MaterialModule } from '@angular/material';
 
-// import { RoutesModule } from './routes.config';
+import { RoutesModule } from './routes.config';
 
 import { PeopleAppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -15,8 +16,9 @@ import { PersonComponent } from './person/person.component';
   imports: [
     BrowserModule,
     HttpModule,
+    RouterModule,
     MaterialModule.forRoot(),
-    // RoutesModule
+    RoutesModule
   ],
   declarations: [
     PeopleAppComponent,
--- src/app/routes.config.ts
+++ src/app/routes.config.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 // APP COMPONENTS
-// import { HomeComponent } ...
+import { HomeComponent } from './home/home.component';
 
 const ROUTES: Routes = [
-  // TODO
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent}
 ];
 
 export const RoutesModule = RouterModule.forRoot(ROUTES,{useHash: true});
