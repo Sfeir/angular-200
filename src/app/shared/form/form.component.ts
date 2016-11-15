f3b82a657ee86d21428138a7c055a938203100dd
Akanass <akanass@icloud.com>
2016-11-15 14:53:13 +0100

    fix custom validation import and use

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Output, Input, EventEmitter, OnChanges } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
+import { CustomValidators } from "./custom-validators";
+
 @Component({
   selector: 'sfeir-form',
   templateUrl: 'form.component.html',
@@ -52,7 +54,9 @@ export class FormComponent implements OnInit, OnChanges {
       lastname: new FormControl('', Validators.compose([
         Validators.required, Validators.minLength(2)
       ])),
-      email: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.compose([
+        Validators.required, CustomValidators.sfeirEmail
+      ])),
       photo: new FormControl('https://randomuser.me/api/portraits/lego/6.jpg'),
       address: new FormGroup({
         street: new FormControl(''),
