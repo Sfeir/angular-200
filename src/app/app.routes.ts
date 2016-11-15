40ffed6ec36b11ce5a0b8e1826abe9b31de53cf6
Akanass <akanass@icloud.com>
2016-11-15 17:48:32 +0100

    fix issue #9

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PeopleComponent } from './people/people.component';
 import { UpdateComponent } from './update/update.component';
+import { PersonComponent } from "./person/person.component";
 
 const ROUTES: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'people', component: PeopleComponent},
-  {path: 'edit/:id', component: UpdateComponent}
+  {path: 'edit/:id', component: UpdateComponent},
+  {path: 'person/:id', component: PersonComponent}
 ];
 
 export const AppRoutes = RouterModule.forRoot(ROUTES,{useHash: true});
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PeopleService } from '../shared/people-service/people.service';
+import { ActivatedRoute } from "@angular/router";
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -11,16 +12,35 @@ const BASE_URL = 'http://localhost:9000';
 export class PersonComponent implements OnInit {
 
   private person: any = {};
+  private _isPerson: boolean;
 
-  constructor( private _service: PeopleService) { }
+  constructor( private _service: PeopleService, private _route: ActivatedRoute) {
+    this._isPerson = false;
+  }
 
   ngOnInit() {
-    this.random();
+    this._route.params.subscribe(params => {
+      const id = params['id'];
+      if (!id) {
+        this.random();
+        this._isPerson = false;
+      }
+      else {
+        this._service.fetchOne(id).subscribe(person => {
+          this.person = person;
+          this._isPerson = true;
+        })
+      }
+    });
   }
 
   random() {
     this._service.fetchRandom()
-      .subscribe((person) => this.person = person);
+        .subscribe((person) => this.person = person);
+  }
+
+  get isPerson(): boolean {
+    return this._isPerson;
   }
 
 }
