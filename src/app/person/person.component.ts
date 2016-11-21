2bb113070a591314b7db32a6a9612d9aec72dc56
Akanass <akanass@icloud.com>
2016-11-21 14:55:53 +0100

    fix issues #47, #50 et #52

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -2,22 +2,40 @@ import { Component, OnInit } from '@angular/core';
 import { PEOPLE } from '../_static/people';
 
 @Component({
-  selector: 'sfeir-person',
-  templateUrl: 'person.component.html',
-  styleUrls: ['person.component.css']
+    selector: 'sfeir-person',
+    templateUrl: 'person.component.html',
+    styleUrls: ['person.component.css']
 })
 export class PersonComponent implements OnInit {
+    // private property to store person value
+    private _person: any;
 
-  private person: any = {};
+    /**
+     * Component constructor
+     */
+    constructor() {
+        this._person = PEOPLE[0];
+    }
 
-  constructor() { }
+    /**
+     * Returns private property _person
+     *
+     * @returns {any}
+     */
+    get person(): any {
+        return this._person;
+    }
 
-  ngOnInit() {
-    this.person = PEOPLE[0];
-  }
+    /**
+     * OnInit implementation
+     */
+    ngOnInit() {
+    }
 
-  random() {
-    this.person = PEOPLE[ (Math.random() * PEOPLE.length) | 0 ];
-  }
-
+    /**
+     * Returns random people
+     */
+    random() {
+        this._person = PEOPLE[ (Math.random() * PEOPLE.length) | 0 ];
+    }
 }
