2657ae0624055b80a6799b85c516fa222d366507
Akanass <akanass@icloud.com>
2016-11-22 17:18:53 +0100

    fix issues #10, #11, #47, #48, #50, #52, #53 et #54

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -1,73 +1,152 @@
 import { Component, OnInit, Output, Input, EventEmitter, OnChanges } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from "@angular/forms";
 
-import { CustomValidators } from './custom-validators';
+import { CustomValidators } from "./custom-validators";
 
 @Component({
-  selector: 'sfeir-form',
-  templateUrl: 'form.component.html',
-  styleUrls: ['form.component.css']
+    selector: 'sfeir-form',
+    templateUrl: 'form.component.html',
+    styleUrls: ['form.component.css']
 })
 export class FormComponent implements OnInit, OnChanges {
+    // private property to store update mode flag
+    private _isUpdateMode: boolean;
+    // private property to store model value
+    private _model:any;
+    // private property to store cancel$ value
+    private _cancel$: EventEmitter<any>;
+    // private property to store submit$ value
+    private _submit$: EventEmitter<any>;
+    // private property to store form value
+    private _form: FormGroup;
 
-  private isUpdateMode: boolean = false;
-
-  @Input() model;
-  @Output('cancel') cancel$;
-  @Output('submit') submit$;
-
-
-  form: FormGroup;
-
-  constructor() {
-    this.submit$ = new EventEmitter();
-    this.cancel$ = new EventEmitter();
-    this.form = this._buildForm();
-    this.model = {address:{}};
-  }
-
-  ngOnInit() {
-  }
-
-  ngOnChanges(record) {
-    if(record.model && record.model.currentValue) {
-      this.model = record.model.currentValue;
-      this.isUpdateMode = !!this.model;
-      this.form.patchValue(this.model);
+    /**
+     * Component constructor
+     */
+    constructor() {
+        this._submit$ = new EventEmitter();
+        this._cancel$ = new EventEmitter();
+        this._model = {address:{}};
+        this._isUpdateMode = false;
+        this._form = this._buildForm();
     }
-  }
 
-  cancel() {
-    this.cancel$.emit();
-  }
+    /**
+     * Sets private property _model
+     *
+     * @param model
+     */
+    @Input() set model(model: any) {
+        this._model = model;
+    }
 
-  submit(person) {
-    this.submit$.emit(person);
-  }
+    /**
+     * Returns private property _model
+     *
+     * @returns {any}
+     */
+    get model(): any {
+        return this._model;
+    }
 
-  private _buildForm() {
-    return new FormGroup({
-      id: new FormControl(''),
-      firstname: new FormControl('', Validators.compose([
-        Validators.required, Validators.minLength(2)
-      ])),
-      lastname: new FormControl('', Validators.compose([
-        Validators.required, Validators.minLength(2)
-      ])),
-      email: new FormControl('', Validators.compose([
-        Validators.required, CustomValidators.sfeirEmail
-      ])),
-      photo: new FormControl('https://randomuser.me/api/portraits/lego/6.jpg'),
-      address: new FormGroup({
-        street: new FormControl(''),
-        city: new FormControl(''),
-        postalCode: new FormControl('')
-      }),
-      phone: new FormControl('', Validators.compose([
-        Validators.required, Validators.pattern('\\d{10}')
-      ])),
-      isManager: new FormControl('')
-    });
-  }
+    /**
+     * Returns private property _form
+     *
+     * @returns {FormGroup}
+     */
+    get form(): FormGroup {
+        return this._form;
+    }
 
+    /**
+     * Returns private property _isUpdateMode
+     *
+     * @returns {boolean}
+     */
+    get isUpdateMode(): boolean {
+        return this._isUpdateMode;
+    }
+
+    /**
+     * Returns private property _cancel$
+     *
+     * @returns {EventEmitter<any>}
+     */
+    @Output('cancel') get cancel$(): EventEmitter<any> {
+        return this._cancel$;
+    }
+
+    /**
+     * Returns private property _submit$
+     *
+     * @returns {EventEmitter<any>}
+     */
+    @Output('submit') get submit$(): EventEmitter<any> {
+        return this._submit$;
+    }
+
+    /**
+     * OnInit implementation
+     */
+    ngOnInit() {
+    }
+
+    /**
+     * Function to handle component update
+     *
+     * @param record
+     */
+    ngOnChanges(record) {
+        if(record.model && record.model.currentValue) {
+            this._model = record.model.currentValue;
+            this._isUpdateMode = !!this._model;
+            this._form.patchValue(this._model);
+        }
+    }
+
+    /**
+     * Function to emit event to cancel process
+     */
+    cancel() {
+        this._cancel$.emit();
+    }
+
+    /**
+     * Function to emit event to submit form and person
+     */
+    submit(person: any) {
+        this._submit$.emit(person);
+    }
+
+    /**
+     * Function to build our form
+     *
+     * @returns {FormGroup}
+     *
+     * @private
+     */
+    private _buildForm(): FormGroup {
+        return new FormGroup({
+            id: new FormControl(''),
+            firstname: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ])),
+            lastname: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ])),
+            email: new FormControl('', Validators.compose([
+                Validators.required, CustomValidators.sfeirEmail
+            ])),
+            photo: new FormControl('https://randomuser.me/api/portraits/lego/6.jpg'),
+            address: new FormGroup({
+                street: new FormControl(''),
+                city: new FormControl(''),
+                postalCode: new FormControl('')
+            }),
+            phone: new FormControl('', Validators.compose([
+                Validators.required, Validators.pattern('\\d{10}')
+            ])),
+            isManager: new FormControl('')
+        });
+    }
 }
