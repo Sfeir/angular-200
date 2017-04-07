37a43935b955b1cd505a30a0af33ed340e4f2004
Wassim CHEGHAM <github@wassimchegham.com>
2017-04-07 11:59:20 +0200

    chore: add pipe specs

--- src/app/shared/na-pipe/na.pipe.spec.ts
+++ src/app/shared/na-pipe/na.pipe.spec.ts
@@ -8,4 +8,33 @@ describe('NaPipe', () => {
     let pipe = new NaPipe();
     expect(pipe).toBeTruthy();
   });
+
+  describe('should return N/A...', () => {
+    it('for NULL', () => {
+      let pipe = new NaPipe();
+      let result = pipe.transform(null);
+      expect(result).toContain('N/A');
+    });
+
+    it('for UNDEFINED', () => {
+      let pipe = new NaPipe();
+      let result = pipe.transform(undefined);
+      expect(result).toContain('N/A');
+    });
+
+    it('for "" (empty string)', () => {
+      let pipe = new NaPipe();
+      let result = pipe.transform('');
+      expect(result).toContain('N/A');
+    });
+  });
+  describe('Should not return N/A', () => {
+    it('for a given manager name: Foo Bar', () => {
+      let pipe = new NaPipe();
+      let result = pipe.transform('Foo Bar');
+      expect(result).toContain('Foo Bar');
+    });
+
+  });
+
 });
