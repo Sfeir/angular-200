c438d61507fb97bd72e24ee2284a21419cc4f6e8
Cyril Balit <cbalit@gmail.com>
2017-04-11 15:16:02 +0200

    clean step-15-solution

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -10,6 +10,7 @@ import { CustomValidators } from "./custom-validators";
 })
 export class FormComponent implements OnInit, OnChanges {
     
+    private form: FormGroup;
     @Input() model:any;
     isUpdateMode: boolean;
 
@@ -20,8 +21,11 @@ export class FormComponent implements OnInit, OnChanges {
     constructor() {
         this.submit$ = new EventEmitter();
         this.cancel$ = new EventEmitter();
-         this.model = {address:{}};
+        this.model = {address:{}};
+        this.form = this._buildForm();
     }
+  
+   
 
     /**
      * OnInit implementation
@@ -38,13 +42,51 @@ export class FormComponent implements OnInit, OnChanges {
         if(record.model && record.model.currentValue) {
             this.model = record.model.currentValue;
             this.isUpdateMode = !!this.model;
+            this.form.patchValue(this.model);
         }
     }
+
+    /**
+     * Function to emit event to cancel process
+     */
     cancel() {
         this.cancel$.emit();
     }
-    
+
+    /**
+     * Function to emit event to submit form and person
+     */
     submit(person: any) {
-        this.submit$.emit(this.model);
+        this.submit$.emit(person);
+    }
+
+    /**
+     * Function to build our form
+     *
+     * @returns {FormGroup}
+     *
+     * @private
+     */
+    private _buildForm(): FormGroup {
+        return new FormGroup({
+            id: new FormControl(''),
+            firstname: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ])),
+            lastname: new FormControl('', Validators.compose([
+                Validators.required, Validators.minLength(2)
+            ])),
+            email: new FormControl('', Validators.required),
+            photo: new FormControl('https://randomuser.me/api/portraits/lego/6.jpg'),
+            address: new FormGroup({
+                street: new FormControl(''),
+                city: new FormControl(''),
+                postalCode: new FormControl('')
+            }),
+            phone: new FormControl('', Validators.compose([
+                Validators.required, Validators.pattern('\\d{10}')
+            ])),
+            isManager: new FormControl('')
+        });
     }
 }
