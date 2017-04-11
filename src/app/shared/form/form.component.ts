c0f919b56548a2cc85232fa41a9f4a35bc215478
Cyril Balit <cbalit@gmail.com>
2017-04-11 14:44:54 +0200

    clean step-13-solution

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -6,15 +6,19 @@ import { FormControl, Validators, FormGroup } from "@angular/forms";
     templateUrl: 'form.component.html',
     styleUrls: ['form.component.css']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, OnChanges {
     
+    @Input() model:any;
+    isUpdateMode: boolean;
+
     @Output('cancel') cancel$: EventEmitter<any>;    
-    @Output('personAdd') add$: EventEmitter<any>;
+    @Output('submit') submit$: EventEmitter<any>;
 
    
     constructor() {
-        this.add$ = new EventEmitter();
+        this.submit$ = new EventEmitter();
         this.cancel$ = new EventEmitter();
+         this.model = {address:{}};
     }
 
     /**
@@ -23,11 +27,22 @@ export class FormComponent implements OnInit {
     ngOnInit() {
     }
 
+    /**
+     * Function to handle component update
+     *
+     * @param record
+     */
+    ngOnChanges(record) {
+        if(record.model && record.model.currentValue) {
+            this.model = record.model.currentValue;
+            this.isUpdateMode = !!this.model;
+        }
+    }
     cancel() {
         this.cancel$.emit();
     }
     
-    add(person: any) {
-        this.add$.emit(person);
+    submit(person: any) {
+        this.submit$.emit(this.model);
     }
 }
