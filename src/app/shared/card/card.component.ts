02439ea8b7e4a186d4878dc08efdc6c782951d0b
Cyril Balit <cbalit@gmail.com>
2017-04-11 13:29:51 +0200

    clean step-09-solution

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -6,33 +6,13 @@ import { Component, OnInit, Input } from '@angular/core';
     styleUrls: ['card.component.css']
 })
 export class CardComponent implements OnInit {
-    // private property to store person value
-    private _person: any;
+    
+    @Input() person: any;
 
-    /**
-     * Component constructor
-     */
     constructor() {
-        this._person = {};
+        this.person = {};
     }
 
-    /**
-     * Returns private property _person
-     *
-     * @returns {any}
-     */
-    get person(): any {
-        return this._person;
-    }
-
-    /**
-     * Sets private property _person
-     *
-     * @param person
-     */
-    @Input() set person(person: any) {
-        this._person = person;
-    }
 
     /**
      * OnInit implementation
