f3524eff744896cb747d0925dba3ea1960bd65ba
Cyril Balit <cbalit@gmail.com>
2017-04-11 13:11:50 +0200

    prepare index, module and main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,27 @@
 // CORE DEPS
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 // MATERIAL DESIGN MODULES
+import 'hammerjs';
 import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { APP_ROUTES } from './app.routes';
 
-import { PeopleAppComponent } from './app.component';
-import { HomeComponent } from './home/index';
-import { PersonComponent } from './person/index';
-import { PeopleComponent } from "./people/index";
-import { CardComponent, FormComponent } from "./shared/index";
-import { UpdateComponent } from "./update/index";
+
+//import { PeopleAppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    HttpModule,
-    RouterModule,
-    ReactiveFormsModule,
-    MaterialModule.forRoot(),
-    APP_ROUTES
+    BrowserAnimationsModule,
+    MaterialModule.forRoot()
   ],
   declarations: [
-    PeopleAppComponent,
-    HomeComponent,
-    PersonComponent,
-    PeopleComponent,
-    CardComponent,
-    FormComponent,
-    UpdateComponent
+    //PeopleAppComponent
   ],
   providers: [ ],
   bootstrap: [
-    PeopleAppComponent
+    //PeopleAppComponent
   ]
 })
 export class AppModule { }
