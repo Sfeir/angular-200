04a9e62f9d6a8f968eec5b367da4526aaba793a5
Cyril Balit <cbalit@gmail.com>
2017-04-11 11:39:58 +0200

    clean step-01-solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
     templateUrl: 'app.component.html',
     styleUrls: ['app.component.css']
 })
-export class PeopleAppComponent {}
+export class PeopleAppComponent {    
+    name:string;
+    
+    constructor() {
+        this.name = 'Angular 2';
+    }
+}
