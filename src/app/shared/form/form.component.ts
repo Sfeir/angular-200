0f2019f831476aab871b9fa88ff6e353c89f5d84
Cyril Balit <cbalit@gmail.com>
2017-04-11 15:20:47 +0200

    clean step-16-solution

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -76,7 +76,9 @@ export class FormComponent implements OnInit, OnChanges {
             lastname: new FormControl('', Validators.compose([
                 Validators.required, Validators.minLength(2)
             ])),
-            email: new FormControl('', Validators.required),
+            email: new FormControl('', Validators.compose([
+                Validators.required, CustomValidators.sfeirEmail
+            ])),
             photo: new FormControl('https://randomuser.me/api/portraits/lego/6.jpg'),
             address: new FormGroup({
                 street: new FormControl(''),
