d38060e91d1b6254435669486865568a5a2083b6
Cyril Balit <cbalit@gmail.com>
2017-04-11 15:39:47 +0200

    clean step-17-solution

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import 'rxjs/add/operator/mergeMap';
 import { Observable } from "rxjs";
-
-const BASE_URL = 'http://localhost:9000';
+import { PeopleService } from "../shared";
 
 @Component({
     selector: 'sfeir-update',
@@ -17,7 +16,7 @@ export class UpdateComponent implements OnInit {
     /**
      * Component constructor
      */
-    constructor(private _route: ActivatedRoute, private _router: Router, private _http: Http) {        
+    constructor(private _route: ActivatedRoute, private _router: Router, private _peopleService: PeopleService) {
         this.person = {
             address: {}
         };
@@ -30,13 +29,12 @@ export class UpdateComponent implements OnInit {
     ngOnInit() {
         this._route.params
             .map((params: any) => params.id)
-            .flatMap((id: string) => this._http.get(`${BASE_URL}/api/peoples/${id}`))
-            .map(res => res.json())            
+            .flatMap((id: string) => this._peopleService.fetchOne(id))             
             .subscribe( (person: any) => this.person = person);
     }
 
     submit(person: any) {
-        this._http.put(`${BASE_URL}/api/peoples/${person.id}`,person)
+        this._peopleService.update(person)
             .subscribe( () => this._router.navigate(['/people']) );
     }
 
