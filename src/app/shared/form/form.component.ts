cc7ec68ac08c965e9c7af1d0ad71d5d6496f6eb9
Cyril Balit <cbalit@gmail.com>
2017-04-11 14:24:45 +0200

    clean step-12-solution

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -5,69 +5,15 @@ import { Component, OnInit, Output, Input, EventEmitter, OnChanges } from '@angu
     templateUrl: 'form.component.html',
     styleUrls: ['form.component.css']
 })
-export class FormComponent implements OnInit, OnChanges {
-    // private property to store update mode flag
-    private _isUpdateMode: boolean;
-    // private property to store model value
-    private _model:any;
-    // private property to store cancel$ value
-    private _cancel$: EventEmitter<any>;
-    // private property to store submit$ value
-    private _submit$: EventEmitter<any>;
+export class FormComponent implements OnInit {
+    
+    @Output('cancel') cancel$: EventEmitter<any>;    
+    @Output('personAdd') add$: EventEmitter<any>;
 
-    /**
-     * Component constructor
-     */
+   
     constructor() {
-        this._submit$ = new EventEmitter();
-        this._cancel$ = new EventEmitter();
-        this._model = {address:{}};
-        this._isUpdateMode = false;
-    }
-
-    /**
-     * Sets private property _model
-     *
-     * @param model
-     */
-    @Input() set model(model: any) {
-        this._model = model;
-    }
-
-    /**
-     * Returns private property _model
-     *
-     * @returns {any}
-     */
-    get model(): any {
-        return this._model;
-    }
-
-    /**
-     * Returns private property _isUpdateMode
-     *
-     * @returns {boolean}
-     */
-    get isUpdateMode(): boolean {
-        return this._isUpdateMode;
-    }
-
-    /**
-     * Returns private property _cancel$
-     *
-     * @returns {EventEmitter<any>}
-     */
-    @Output('cancel') get cancel$(): EventEmitter<any> {
-        return this._cancel$;
-    }
-
-    /**
-     * Returns private property _submit$
-     *
-     * @returns {EventEmitter<any>}
-     */
-    @Output('submit') get submit$(): EventEmitter<any> {
-        return this._submit$;
+        this.add$ = new EventEmitter();
+        this.cancel$ = new EventEmitter();
     }
 
     /**
@@ -76,29 +22,11 @@ export class FormComponent implements OnInit, OnChanges {
     ngOnInit() {
     }
 
-    /**
-     * Function to handle component update
-     *
-     * @param record
-     */
-    ngOnChanges(record) {
-        if(record.model && record.model.currentValue) {
-            this._model = record.model.currentValue;
-            this._isUpdateMode = !!this._model;
-        }
-    }
-
-    /**
-     * Function to emit event to cancel process
-     */
     cancel() {
-        this._cancel$.emit();
+        this.cancel$.emit();
     }
-
-    /**
-     * Function to emit event to submit form and person
-     */
-    submit() {
-        this._submit$.emit(this._model);
+    
+    add(person: any) {
+        this.add$.emit(person);
     }
 }
