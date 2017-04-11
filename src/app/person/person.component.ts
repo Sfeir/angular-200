80798216bbc12064a37d90bbc129b718775db6a0
Cyril Balit <cbalit@gmail.com>
2017-04-11 12:41:22 +0200

    clean step-04-solution

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -7,38 +7,13 @@ import { PeopleService } from "../shared/index";
     templateUrl: 'person.component.html',
     styleUrls: ['person.component.css']
 })
-export class PersonComponent implements OnInit {
-    // private property to store person value
-    private _person: any;
-    // private property to store flag to know if it's a person
-    private _isPerson: boolean;
-
-    /**
-     * Component constructor
-     */
-    constructor(private _peopleService: PeopleService, private _route: ActivatedRoute) {
-        this._person = {};
-        this._isPerson = false;
+export class PersonComponent implements OnInit {    
+    private person: any;
+    
+    constructor() {
+        this.person = PEOPLE[0];
     }
-
-    /**
-     * Returns flag to know if we are on a profil or on HP
-     *
-     * @returns {boolean}
-     */
-    get isPerson(): boolean {
-        return this._isPerson;
-    }
-
-    /**
-     * Returns private property _person
-     *
-     * @returns {any}
-     */
-    get person(): any {
-        return this._person;
-    }
-
+    
     /**
      * OnInit implementation
      */
