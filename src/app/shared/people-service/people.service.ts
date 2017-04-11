fa9a828fbdda4d2fcb6d9cdc9fd6681a8fd2ba78
Cyril Balit <cbalit@gmail.com>
2017-04-11 15:39:47 +0200

    clean step-17-solution

--- src/app/shared/people-service/people.service.ts
+++ src/app/shared/people-service/people.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers, Response, RequestOptions } from '@angular/http';
-import 'rxjs/add/operator/map';
 import { Observable } from "rxjs";
 import { environment } from "../../../environments/environment";
 
 @Injectable()
 export class PeopleService {
-    // private property to store all backend URLs
+    
     private _backendURL: any;
 
-    /**
-     * Service constructor
-     */
+    
     constructor(private _http: Http) {
         this._backendURL = {};
 
@@ -25,107 +22,36 @@ export class PeopleService {
         Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[k] = `${baseUrl}${environment.backend.endpoints[k]}`);
     }
 
-    /**
-     * Function to return list of people
-     *
-     * @returns {Observable<R>}
-     */
     fetch(): Observable<any[]> {
-        return this._http.get(this._backendURL.allPeople, this._options())
-            .map((res: Response) => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                else {
-                    return [];
-                }
-            });
+        return this._http.get(this._backendURL.allPeople)
+            .map(res => res.json());
     }
-
-    /**
-     * Function to return one random person from people list
-     *
-     * @returns {Observable<R>}
-     */
+    
     fetchRandom(): Observable<any> {
-        return this._http.get(this._backendURL.randomPeople, this._options())
-            .map((res: Response) => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                else {
-                    return {};
-                }
-            });
+        return this._http.get(this._backendURL.randomPeople)
+            .map(res => res.json());
     }
 
-    /**
-     * Function to return one person for current id
-     *
-     * @param id
-     *
-     * @returns {Observable<R>}
-     */
     fetchOne(id: string): Observable<any> {
-        return this._http.get(this._backendURL.onePeople.replace(':id', id), this._options())
-            .map((res: Response) => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                else {
-                    return {};
-                }
-            });
+        return this._http.get(this._backendURL.onePeople.replace(':id', id))
+            .map(res => res.json());
     }
 
-    /**
-     * Function to delete one person for current id
-     *
-     * @param id
-     *
-     * @returns {Observable<R>}
-     */
     delete(id: string): Observable<any[]> {
-        return this._http.delete(this._backendURL.onePeople.replace(':id', id), this._options())
-            .map((res: Response) => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                else {
-                    return [];
-                }
-            });
+        return this._http.delete(this._backendURL.onePeople.replace(':id', id))
+            .map(res => res.json());
     }
 
-    /**
-     * Function to update one person
-     *
-     * @param person
-     *
-     * @returns {Observable<R>}
-     */
     update(person: any): Observable<any> {
-        return this._http.put(this._backendURL.onePeople.replace(':id', person.id), person, this._options())
+        return this._http.put(this._backendURL.onePeople.replace(':id', person.id), person)
             .map((res: Response) => res.json());
     }
 
-    /**
-     * Function to create a new person
-     *
-     * @param person
-     *
-     * @returns {Observable<R>}
-     */
     create(person): Observable<any> {
         return this._http.post(this._backendURL.allPeople, JSON.stringify(person), this._options())
             .map((res: Response) => res.json());
     }
 
-    /**
-     * Function to return request options
-     *
-     * @returns {RequestOptions}
-     */
     private _options(headerList: Object = {}): RequestOptions {
         const headers = new Headers(Object.assign({'Content-Type': 'application/json'}, headerList));
         return new RequestOptions({headers: headers});
