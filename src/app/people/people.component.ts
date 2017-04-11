0ea4d4069989310f34940a6d366aabfe8eb48169
Cyril Balit <cbalit@gmail.com>
2017-04-11 13:19:17 +0200

    clean step-08-solution

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
-import { environment } from "../../environments/environment";
+
+const BASE_URL = 'http://localhost:9000';
 
 @Component({
     selector: 'sfeir-people',
@@ -8,68 +9,17 @@ import { environment } from "../../environments/environment";
     styleUrls: ['people.component.css']
 })
 export class PeopleComponent implements OnInit {
-    // private property to store people value
-    private _people: any[];
-    // private property to store all backend URLs
-    private _backendURL: any;
-
-    /**
-     * Component constructor
-     */
-    constructor(private _http: Http) {
-        this._people = [];
-        this._backendURL = {};
-
-        // build backend base url
-        let baseUrl = `${environment.backend.protocol}://${environment.backend.host}`;
-        if (environment.backend.port) {
-            baseUrl += `:${environment.backend.port}`;
-        }
-
-        // build all backend urls
-        Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[k] = `${baseUrl}${environment.backend.endpoints[k]}`);
-    }
-
-    /**
-     * Returns private property _people
-     *
-     * @returns {any[]}
-     */
-    get people(): any[] {
-        return this._people;
-    }
+    
+    private people: any[];
+    
+    constructor(private _http: Http) {}    
 
     /**
      * OnInit implementation
      */
     ngOnInit() {
-        this._http.get(this._backendURL.allPeople)
-            .map( res => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                else {
-                    return [];
-                }
-            })
-            .subscribe( (people: any[]) => this._people = people);
-    }
-
-    /**
-     * Function to delete on person
-     *
-     * @param person
-     */
-    delete(person: any) {
-        this._http.delete(this._backendURL.onePeople.replace(':id', person.id))
-            .map( res => {
-                if (res.status === 200) {
-                    return res.json();
-                }
-                else {
-                    return [];
-                }
-            })
-            .subscribe( (people: any[]) => this._people = people);
+        this._http.get(`${BASE_URL}/api/peoples/`)
+            .map(res => res.json())            
+            .subscribe( (people: any[]) => this.people = people);
     }
 }
