15737a13535e2fabc115177f166a488343cec7ff
Cyril Balit <cbalit@gmail.com>
2017-04-11 13:11:50 +0200

    prepare index, module and main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,52 +1,26 @@
 // CORE DEPS
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 // MATERIAL DESIGN MODULES
+import 'hammerjs';
 import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { APP_ROUTES } from './app.routes';
 
-import { PeopleAppComponent } from './app.component';
-import { PersonComponent } from './person/index';
-import { PeopleComponent } from "./people/index";
-import { CardComponent, FormComponent, PeopleService } from "./shared/index";
-import { NaPipe } from './shared/na-pipe/na.pipe';
-import { SfeirBadgeDirective } from './shared/badge/sfeir-badge.directive';
-import { HomeComponent } from './home/home.component';
-import { UpdateComponent } from './update/update.component';
+
+//import { PeopleAppComponent } from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    HttpModule,
-    RouterModule,
-    ReactiveFormsModule,
-    MaterialModule.forRoot(),
-    APP_ROUTES
+    BrowserAnimationsModule,
+    MaterialModule.forRoot()
   ],
   declarations: [
-    PeopleAppComponent,
-    HomeComponent,
-    PersonComponent,
-    PeopleComponent,
-    CardComponent,
-    FormComponent,
-    UpdateComponent,
-    NaPipe,
-    SfeirBadgeDirective,
-    NaPipe,
-    SfeirBadgeDirective,
-    HomeComponent,
-    UpdateComponent
-  ],
-  providers: [
-    PeopleService
+    //PeopleAppComponent
   ],
   bootstrap: [
-    PeopleAppComponent
+    //PeopleAppComponent
   ]
 })
 export class AppModule { }
