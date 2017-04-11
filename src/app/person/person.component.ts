f96a9912ccf17e73739f75cc7a51d85e698f65a4
Cyril Balit <cbalit@gmail.com>
2017-04-11 12:41:22 +0200

    clean step-04-solution

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -6,26 +6,13 @@ import { PEOPLE } from '../_static/people';
     templateUrl: 'person.component.html',
     styleUrls: ['person.component.css']
 })
-export class PersonComponent implements OnInit {
-    // private property to store person value
-    private _person: any;
-
-    /**
-     * Component constructor
-     */
+export class PersonComponent implements OnInit {    
+    private person: any;
+    
     constructor() {
-        this._person = PEOPLE[0];
+        this.person = PEOPLE[0];
     }
-
-    /**
-     * Returns private property _person
-     *
-     * @returns {any}
-     */
-    get person(): any {
-        return this._person;
-    }
-
+    
     /**
      * OnInit implementation
      */
