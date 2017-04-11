b0b7fe499a3625ca8450055dd62020d2ee1a58cb
Cyril Balit <cbalit@gmail.com>
2017-04-11 13:19:17 +0200

    clean step-08-solution

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from "@angular/router";
-import 'rxjs/add/operator/mergeMap';
-import { PeopleService } from "../shared/index";
+import { Http } from '@angular/http';
+
+const BASE_URL = 'http://localhost:9000';
 
 @Component({
     selector: 'sfeir-people',
@@ -9,30 +9,10 @@ import { PeopleService } from "../shared/index";
     styleUrls: ['./people.component.css']
 })
 export class PeopleComponent implements OnInit {
-    // private property to store people value
-    private _people: any[];
-    // private property to store dialogStatus value
-    private _dialogStatus: string;
-    // private property to store view value
-    private _view: string;
-
-    /**
-     * Component constructor
-     */
-    constructor(private _router: Router, private _peopleService: PeopleService) {
-        this._people = [];
-        this._dialogStatus = 'inactive';
-        this._view = 'card';
-    }
-
-    /**
-     * Returns private property _people
-     *
-     * @returns {any[]}
-     */
-    get people(): any[] {
-        return this._people;
-    }
+    
+    private people: any[];
+    
+    constructor(private _http: Http) {}    
 
     /**
      * Returns private property _dialogStatus
@@ -56,59 +36,8 @@ export class PeopleComponent implements OnInit {
      * OnInit implementation
      */
     ngOnInit() {
-        this._peopleService.fetch().subscribe((people: any[]) => this._people = people);
-    }
-
-    /**
-     * Function to delete one person
-     *
-     * @param person
-     */
-    delete(person: any) {
-        this._peopleService.delete(person.id).subscribe((people: any[]) => this._people = people);
-    }
-
-    /**
-     * Function to add one person
-     *
-     * @param person
-     */
-    add(person: any) {
-        this._peopleService.create(person)
-            .flatMap(_ => this._peopleService.fetch())
-            .subscribe((people: any[]) => {
-                this._people = people;
-                this.hideDialog();
-            });
-    }
-
-    /**
-     * Function to display modal
-     */
-    showDialog() {
-        this._dialogStatus = 'active';
-    }
-
-    /**
-     * Function to hide modal
-     */
-    hideDialog() {
-        this._dialogStatus = 'inactive';
-    }
-
-    /**
-     * Function to switch view
-     */
-    switchView() {
-        this._view = (this._view === 'card') ? 'list' : 'card';
-    }
-
-    /**
-     * Function to navigate to current person
-     *
-     * @param person
-     */
-    navigate(person) {
-        this._router.navigate(['/person', person.id]);
+        this._http.get(`${BASE_URL}/api/peoples/`)
+            .map(res => res.json())            
+            .subscribe( (people: any[]) => this.people = people);
     }
 }
