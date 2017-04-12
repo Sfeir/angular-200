ed8668a4acf5966d37f3947a264543786def9c82
Cyril Balit <cbalit@gmail.com>
2017-04-12 10:12:58 +0200

    clean step-21-solution

--- src/app/shared/badge/sfeir-badge.directive.ts
+++ src/app/shared/badge/sfeir-badge.directive.ts
@@ -4,28 +4,20 @@ import { Directive, ElementRef, Renderer, Input, OnInit } from '@angular/core';
     selector: '[sfeir-badge]'
 })
 export class SfeirBadgeDirective implements OnInit {
-    // private property to store person value
-    private _person: any;
+    
+    @Input() person: any;
 
     /**
      * Component constructor
      */
     constructor(private _el: ElementRef, private _rd: Renderer) { }
-
-    /**
-     * Sets private property _person
-     *
-     * @param person
-     */
-    @Input() set person(person: any) {
-        this._person = person;
-    };
+    
 
     /**
      * OnInit implementation
      */
     ngOnInit() {
-        if(this._person && this._person.isManager) {
+        if(this.person && this.person.isManager) {
             this._rd.setElementProperty(this._el.nativeElement, 'innerHTML', '<i class="material-icons">supervisor_account</i>');
         }
     }
