9e9f210d80c1d26b4089741874f96163ae805c6e
Cyril Balit <cbalit@gmail.com>
2017-10-02 15:26:13 +0200

    update feature

--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute, Router } from '@angular/router';
+import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
 const BASE_URL = 'http://localhost:9000';
 
@@ -11,28 +12,25 @@ const BASE_URL = 'http://localhost:9000';
   templateUrl: 'update.component.html',
   styleUrls: ['update.component.css']
 })
-export class UpdateComponent implements OnInit {    
+export class UpdateComponent implements OnInit {
     person: any;
-    
 
     /**
      * Component constructor
      */
-    constructor(private _route: ActivatedRoute, private _router: Router, private _http: Http) {        
+    constructor(private _route: ActivatedRoute, private _router: Router, private _http: HttpClient) {
         this.person = {
             address: {}
         };
     }
 
-   
     /**
      * OnInit implementation
      */
     ngOnInit() {
         this._route.params
             .map((params: any) => params.id)
-            .flatMap((id: string) => this._http.get(`${BASE_URL}/api/peoples/${id}`))
-            .map(res => res.json())            
+            .mergeMap((id: string) => this._http.get(`${BASE_URL}/api/peoples/${id}`))
             .subscribe( (person: any) => this.person = person);
     }
 
