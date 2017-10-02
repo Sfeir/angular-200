43ea8b045c3e3f4c4ff82cad58da76ccf424b476
Cyril Balit <cbalit@gmail.com>
2017-10-02 13:48:04 +0200

    add HttpClient dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 // CORE DEPS
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 // MATERIAL DESIGN MODULES
 import {
   MdToolbarModule,
@@ -39,7 +37,8 @@ import { UpdateComponent } from './update';
     MdCheckboxModule,
     MdRadioModule,
     MdIconModule,
-    MdListModule
+    MdListModule,
+    HttpClientModule
   ],
   declarations: [
     PeopleAppComponent,
