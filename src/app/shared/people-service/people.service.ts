27133fd568ac3ff250219ce1537f11986e4c5304
Cyril Balit <cbalit@gmail.com>
2017-10-02 15:49:41 +0200

    http in service

--- src/app/shared/people-service/people.service.ts
+++ src/app/shared/people-service/people.service.ts
@@ -1,15 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, Response, RequestOptions } from '@angular/http';
-import { Observable } from "rxjs";
-import { environment } from "../../../environments/environment";
+import { HttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
 export class PeopleService {
-    
+
     private _backendURL: any;
 
-    
-    constructor(private _http: Http) {
+    constructor(private _http: HttpClient) {
         this._backendURL = {};
 
         // build backend base url
@@ -22,38 +21,27 @@ export class PeopleService {
         Object.keys(environment.backend.endpoints).forEach(k => this._backendURL[k] = `${baseUrl}${environment.backend.endpoints[k]}`);
     }
 
-    fetch(): Observable<any[]> {
-        return this._http.get(this._backendURL.allPeople)
-            .map(res => res.json());
+    fetch(): Observable<any> {
+        return this._http.get(this._backendURL.allPeople);
     }
-    
+
     fetchRandom(): Observable<any> {
-        return this._http.get(this._backendURL.randomPeople)
-            .map(res => res.json());
+        return this._http.get(this._backendURL.randomPeople);
     }
 
     fetchOne(id: string): Observable<any> {
-        return this._http.get(this._backendURL.onePeople.replace(':id', id))
-            .map(res => res.json());
+        return this._http.get(this._backendURL.onePeople.replace(':id', id));
     }
 
-    delete(id: string): Observable<any[]> {
-        return this._http.delete(this._backendURL.onePeople.replace(':id', id))
-            .map(res => res.json());
+    delete(id: string): Observable<any> {
+        return this._http.delete(this._backendURL.onePeople.replace(':id', id));
     }
 
     update(person: any): Observable<any> {
-        return this._http.put(this._backendURL.onePeople.replace(':id', person.id), person)
-            .map((res: Response) => res.json());
+        return this._http.put(this._backendURL.onePeople.replace(':id', person.id), person);
     }
 
     create(person): Observable<any> {
-        return this._http.post(this._backendURL.allPeople, JSON.stringify(person), this._options())
-            .map((res: Response) => res.json());
-    }
-
-    private _options(headerList: Object = {}): RequestOptions {
-        const headers = new Headers(Object.assign({'Content-Type': 'application/json'}, headerList));
-        return new RequestOptions({headers: headers});
+        return this._http.post(this._backendURL.allPeople, person);
     }
 }
