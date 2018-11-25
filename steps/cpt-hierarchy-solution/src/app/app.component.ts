b2a1eea4cba8949299c1d1f8a16e80980959fd98
Noel Mace <contact@noelmace.com>
2018-11-25 21:53:16 +0100

    feat(cpt-hierarchy): create solution from branch step-03-solution

---
+++ steps/cpt-hierarchy-solution/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'sfeir-app',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
+})
+export class PeopleAppComponent {
+
+    constructor() {}
+}
